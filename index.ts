d031a52117ad9fe3031fab0691f44036ce361aa0
Alejandro Freire <alejandrofreire@Alejandros-MacBook-Air.local>
2024-07-27 23:05:46 -0400

    changed port

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import OpenAI  from 'openai'
-const PORT: number = 8000
+const PORT: number = 5000
 
 const app: Application = express()
 app.use(cors())
